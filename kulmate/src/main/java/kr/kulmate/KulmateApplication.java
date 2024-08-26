4b9289eab42086e920340833a4d0bdafe8a4115f
chabin37 <7chabin@naver.com>
2024-08-27 02:30:56 +0900

    1

---
+++ kulmate/src/main/java/kr/kulmate/KulmateApplication.java
@@ -0,0 +1,13 @@
+package kr.kulmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KulmateApplication.class, args);
+	}
+
+}
---
+++ kulmate/src/test/java/kr/kulmate/KulmateApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kulmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
